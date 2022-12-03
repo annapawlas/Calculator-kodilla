8dbf097384520b64a26d9d8dd498d7bf84d8452b
Anna Pawlas <annapawlas1@gmail.com>
2022-12-03 23:11:32 +0100

    Commit

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,12 @@
+package com.kodilla;
+
+public class Calculator {
+
+    public double addAToB(double a, double b) {
+        return a + b;
+    }
+
+    public double subtractAFromB(double a, double b) {
+        return b - a;
+    }
+}
---
+++ src/main/java/com/kodilla/CalculatorKodillaApplication.java
@@ -0,0 +1,19 @@
+package com.kodilla;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+public class CalculatorKodillaApplication {
+
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.addAToB(2.5, 3));
+
+    }
+
+
+
+}
